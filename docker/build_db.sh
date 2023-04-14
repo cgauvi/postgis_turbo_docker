a3e7e82b54f5a9dfda60c4e0228280c4375490da
cgauvi <charles.gauvin@lacapitale.com>
2023-04-14 10:12:41 -0400

    converted to unix files + new scripts tot ry to build libposal from local downlad tar.gz

--- docker/build_db.sh
+++ docker/build_db.sh
@@ -1,5 +1,6 @@
- #! bin/bash 
+ #! /bin/bash 
  
+# Download repo
 if [ ! -d ./libpostal ]; then
     git clone --depth 1 --branch v1.0.0 https://github.com/openvenues/libpostal ./libpostal
     find ./libpostal -type f -print0 | xargs -0 dos2unix
@@ -7,13 +8,19 @@ else
     echo "Already cloned repo libpostal.."
 fi
 
+# Download data
+chmod u+x ./dl_postal_lib.sh
+dos2unix ./dl_postal_lib.sh
+./dl_postal_lib.sh
+
+
+# Download pgsql
 if [ ! -d ./pgsql-postal ]; then
     git clone --depth 1 --branch master https://github.com/pramsey/pgsql-postal ./pgsql-postal
     find ./pgsql-postal -type f -print0 | xargs -0 dos2unix
 else
     echo "Already cloned repo pgsql-postal.."
 fi
-
 
 
 docker compose -f docker-compose.yml --env-file ./config/.env build db
--- docker/build_libpostal.sh
+++ docker/build_libpostal.sh
@@ -11,23 +11,3 @@ make install
 ldconfig
  
 
-curl -L https://github.com/openvenues/libpostal/releases/download/v1.0.0/parser.tar.gz -o $DATA_DIR/parser.tar.gz 
-curl -L https://github.com/openvenues/libpostal/releases/download/v1.0.0/libpostal_data.tar.gz -o $DATA_DIR/libpostal_data.tar.gz 
-curl -L https://github.com/openvenues/libpostal/releases/download/v1.0.0/language_classifier.tar.gz o $DATA_DIR/language_classifier.tar.gz 
-
-
-mkdir -p $DATA_DIR/parser
-tar -xvzf $DATA_DIR/parser.tar.gz  -C $DATA_DIR/parser
-
-mkdir -p $DATA_DIR/libpostal_data
-tar -xvzf $DATA_DIR/libpostal_data.tar.gz  -C $DATA_DIR/libpostal_data
-
-mkdir -p $DATA_DIR/language_classifier
-tar -xvzf $DATA_DIR/language_classifier.tar.gz  -C $DATA_DIR/language_classifier
-
-echo $(date -u) > $DATA_DIR/last_updated
-echo $(date -u) > $DATA_DIR/last_updated_parser
-echo $(date -u) > $DATA_DIR/last_updated_language_classifier
-
-
-./src/address_parser
